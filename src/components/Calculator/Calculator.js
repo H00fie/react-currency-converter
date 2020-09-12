65505420ae4cfa4dcdf1006982be45cdbc3b54ed
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-09-12 12:52:26 +0200

    Added Select function.

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 
+function Select({ value, setCurrency }){
+    return (
+        <select value={value} onChange={(event) => setCurrency(event.target.value)}>
+            <option value="USD">USD</option>
+            <option value="PLN">PLN</option>
+            <option value="GBP">GBP</option>
+            <option value="EUR">EUR</option>
+        </select>
+    );
+}
+
 function Calculator() {
     const [amount, setAmount] = useState(0);
     const [currencyFrom, setCurrencyFrom] = useState('PLN');
@@ -14,26 +25,15 @@ function Calculator() {
             </div>
             <div>
                 <span>From:</span>
-                <select value={currencyFrom} onChange={(event) => console.log(event.target.value)}>
-                    <option value="USD">USD</option>
-                    <option value="PLN">PLN</option>
-                    <option value="GBP">GBP</option>
-                    <option value="EUR">EUR</option>
-                </select>
+                <Select value={currencyFrom} setCurrency={setCurrencyFrom}/>
             </div>
             <div>
                 <span>To:</span>
-                <select>
-                    <option value="USD">USD</option>
-                    <option value="PLN">PLN</option>
-                    <option value="GBP">GBP</option>
-                    <option value="EUR">EUR</option>
-                </select>
+                <Select value={currencyTo} setCurrency={setCurrencyTo}/>
             </div>
             <div>Result: {amount}</div>
         </form>
     )
-
 }
 
 export default Calculator;
