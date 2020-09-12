6fa9fbccf2b5896b4da453852d3c259d6593de73
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-09-12 10:46:29 +0200

    First commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const day = "Starday";
+
+function App() {
+  return (
+    <div class="App">
+      <p>Hello!</p>
+      <p>{day}</p>
+        <button
+    </div>
+  );
+}
+
+export default App;
